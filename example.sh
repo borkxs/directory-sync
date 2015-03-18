db4ba730966790a9a393a39053b98ec4d28a74e1
Erik Michaelson <erik.michaelson@gmail.com>
2015-03-18 14:24:56 -0500

    add --delete option to rsync so target directory doesn't keep removed files

--- example.sh
+++ example.sh
@@ -5,7 +5,7 @@ declare -a directories=("some_directory1" "some_directory2" "some_directory3")
 for i in "${directories[@]}"
 do
     echo Updating directory $i...
-    /usr/local/bin/rsync -a --no-o --no-p --exclude-from /Users/emichaelson/Code/trunk.professional_services/exclude.txt /Users/emichaelson/Code/trunk.professional_services/$i/ /Volumes/C/Users/emichaelson/sync/$i
+    /usr/local/bin/rsync -a --no-o --no-p --delete --exclude-from /Users/emichaelson/Code/trunk.professional_services/exclude.txt /Users/emichaelson/Code/trunk.professional_services/$i/ /Volumes/C/Users/emichaelson/sync/$i
 done
 
 forever start /Users/emichaelson/Code/trunk.professional_services/directory-sync/watch.js ${directories[@]}
